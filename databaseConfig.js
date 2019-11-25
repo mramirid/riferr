a3cfc0c79a54899fad0f2e6e2b24e61db1409c0c
kholilboy <kholil.zboy@gmail.com>
2019-11-25 23:59:17 +0700

    config sukses koneksi db

--- databaseConfig.js
+++ databaseConfig.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host     : '127.0.0.1',
     user     : 'root',
     password : '',
-    database : 'addawdawd'
+    database : 'riferr'
 });
 
 connection.connect(function(err) {
