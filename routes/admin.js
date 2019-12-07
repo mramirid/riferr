0f7a4d2067ee5206731ce3be8ad7f0c2809af51c
kholilboy <kholil.zboy@gmail.com>
2019-12-07 20:39:46 +0700

    tes

--- routes/admin.js
+++ routes/admin.js
@@ -10,22 +10,6 @@ router.post('/auth', function(request, response) {
 	var username = request.body.u_adm;
 	var password = request.body.p_adm;
 	if (username && password) {
-<<<<<<< HEAD
-		a = connection.query('SELECT * FROM seller WHERE nama_seller = ? AND psswrd_seller = ?', [username, password], function(error, results, fields){
-			if (results.length > 0) {
-				// response.send('nyoba');
-				response.redirect('/dashboard');
-			}else{
-				b = connection.query('SELECT * FROM buyer WHERE nama_buyer = ? AND psswrd_buyer = ?', [username, password], function(error, results, fields) {
-					if( results.length > 0){
-						response.send('AAAA');
-					}else{
-						response.send('pwd / email salah');
-					}
-				});
-			}		
-			response.end();
-=======
 		connection.query('SELECT * FROM seller WHERE sellernickname = ? AND sellerpassword = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
 				// response.send('nyoba');
@@ -42,7 +26,6 @@ router.post('/auth', function(request, response) {
 					}
 				});
 			}
->>>>>>> f8f01441a80ec44ffd003a899473f0aed15074e5
 		});
 	
 	} else {
