f8f01441a80ec44ffd003a899473f0aed15074e5
FJDHIKA <fajarhandika12@gmail.com>
2019-12-07 20:30:11 +0700

    lel

--- routes/admin.js
+++ routes/admin.js
@@ -10,14 +10,22 @@ router.post('/auth', function(request, response) {
 	var username = request.body.u_adm;
 	var password = request.body.p_adm;
 	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		connection.query('SELECT * FROM seller WHERE sellernickname = ? AND sellerpassword = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
 				// response.send('nyoba');
 				response.redirect('/dashboard');
 			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
+				// response.send('Incorrect Username and/or Password!');
+				connection.query('SELECT * FROM buyer WHERE buyernickname = ? AND buyerpassword = ?', [username, password], function(error, results, fields){
+					if(results.length > 0){
+						response.send('login as buyer');
+						response.end();
+					}else{
+						response.send('aoisndn')
+						response.end();
+					}
+				});
+			}
 		});
 	} else {
 		response.send('Please enter Username and Password!');
